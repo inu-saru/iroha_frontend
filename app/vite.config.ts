99b3c7bdb74b5aa492b3fe97ba8d9f2d1881c56a
inu_saru <inu_saru_eagle@yahoo.co.jp>
2023-03-27 22:33:26 +0900

    3ec47854 add: setting @ alias

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }]
+  },
   plugins: [react()],
   server: {
     host: true,
-    port: 8080,
+    port: 8080
   }
 })
