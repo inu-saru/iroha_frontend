31427aa45c33944254d2411620882c0358cc093d
inu_saru <inu_saru_eagle@yahoo.co.jp>
2023-05-07 22:07:06 +0900

    a3817ee4 add: DropDown component

---
+++ app/src/components/DropDown/DropDown.tsx
@@ -0,0 +1,31 @@
+import { useRef } from "react"
+import { useClickAway, useToggle } from "react-use"
+
+interface DropDownProps {
+  trigger: React.ReactNode
+  children: React.ReactNode
+}
+
+export const DropDown = ({ trigger, children }: DropDownProps): JSX.Element => {
+  const [on, toggle] = useToggle(false)
+  const ref = useRef(null)
+  useClickAway(ref, () => {
+    toggle()
+  })
+
+  return (
+    <div className="relative">
+      <div onClick={toggle} className="inline-block">
+        {trigger}
+      </div>
+      {on && (
+        <ul
+          ref={ref}
+          className="z-50 absolute left-0 top-6 bg-white rounded-sm shadow inline-block py-2 text-default text-primary-200"
+        >
+          {children}
+        </ul>
+      )}
+    </div>
+  )
+}
