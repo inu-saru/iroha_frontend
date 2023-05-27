d26d0319bf86caa83db91d635c067877611e7f7c
inu_saru <inu_saru_eagle@yahoo.co.jp>
2023-05-27 19:06:01 +0900

    a3817ee4 add: userDisclosure hooks

---
+++ app/src/hooks/useDisclosure.ts
@@ -0,0 +1,40 @@
+import * as React from "react"
+
+export const useDisclosure = (initial = false) => {
+  const [isOpen, setIsOpen] = React.useState(initial)
+  const [targetData, setTargetData] = React.useState({})
+
+  const open = React.useCallback(() => {
+    setIsOpen(true)
+  }, [])
+  const close = React.useCallback(() => {
+    setIsOpen(false)
+  }, [])
+  const toggle = React.useCallback(() => {
+    setIsOpen((state) => !state)
+  }, [])
+
+  const openWith = React.useCallback((data: object) => {
+    open()
+    setTargetData(data)
+  }, [])
+  const closeWith = React.useCallback(() => {
+    close()
+    setTargetData({})
+  }, [])
+  const toggleWith = React.useCallback((data: object = {}) => {
+    toggle()
+    setTargetData(data)
+  }, [])
+
+  return {
+    isOpen,
+    open,
+    close,
+    toggle,
+    targetData,
+    openWith,
+    closeWith,
+    toggleWith
+  }
+}
