117f673e6bf1b525204aed1975b54de19a938b79
inu_saru <inu_saru_eagle@yahoo.co.jp>
2023-08-10 21:32:50 +0900

    f19ee6a9 update: set show all button as an temporary expedient

--- app/src/components/List/ListItems.tsx
+++ app/src/components/List/ListItems.tsx
@@ -50,13 +50,19 @@ export const ListItems = ({
           ))
         )}
       </ul>
+      {/* FIXME: 初回レンダリング時にbuttonのrefが取得できないためinfinite scrollが反応しない。show allボタンを設置しているが理想はボタンが不要にしたい */}
       {resourcesQuery.hasNextPage && (
-        <button
-          ref={loadMoreButtonRef}
-          onClick={async () => await resourcesQuery.fetchNextPage()}
-        >
-          {resourcesQuery.isFetchingNextPage ? "isLoading..." : "Load"}
-        </button>
+        <div>
+          {!resourcesQuery.isFetchingNextPage && (
+            <button
+              className="block mx-auto -mt-3 px-2 py-1 text-small border border-solid border-natural-50 rounded-xl bg-white hover:bg-primary-20"
+              ref={loadMoreButtonRef}
+              onClick={async () => await resourcesQuery.fetchNextPage()}
+            >
+              Show all
+            </button>
+          )}
+        </div>
       )}
       <ListFooter count={`${resourcesQuery.data?.pages[0].totalCount}`} />
     </>
