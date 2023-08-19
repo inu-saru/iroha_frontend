ac22b3ec56021ad2c104f35e2cc3c4300503b806
inu_saru <inu_saru_eagle@yahoo.co.jp>
2023-08-19 18:13:31 +0900

    cc81cc6e refactor: sort out OuterSpaceNav

--- app/src/features/misc/components/OuterSpaceNav.tsx
+++ app/src/features/misc/components/OuterSpaceNav.tsx
@@ -1,6 +1,6 @@
 import { Spaces } from "@/features/spaces"
 
-export const Nav = (): JSX.Element => {
+export const OuterSpaceNav = (): JSX.Element => {
   return (
     <div className="w-56 border-r border-natural-40 h-screen bg-white overflow-y-auto">
       <div className="p-2 border-b border-natural-40">
--- app/src/features/nav/index.ts
+++ app/src/features/nav/index.ts
@@ -1,2 +1,1 @@
-export * from "./components/Nav"
 export * from "./components/SpaceNav"
