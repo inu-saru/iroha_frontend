e2105fd7c2abf17e69f8b400ded2afcd9cfcf7aa
inu_saru <inu_saru_eagle@yahoo.co.jp>
2023-08-29 21:53:59 +0900

    d782f7ed update: reinstall storybook v7

--- app/.storybook/main.ts
+++ app/.storybook/main.ts
@@ -1,45 +1,19 @@
-const path = require("path")
-const { loadConfigFromFile, mergeConfig } = require("vite")
-const svgrPlugin = require("vite-plugin-svgr")
+import type { StorybookConfig } from "@storybook/react-vite"
 
-module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "@storybook/addon-mdx-gfm"
   ],
-
   framework: {
     name: "@storybook/react-vite",
-    options: {}
+    options: {},
   },
-
-  features: {
-    storyStoreV7: true
-  },
-
-  async viteFinal(config, { configType }) {
-    const { config: userConfig } = await loadConfigFromFile(
-      path.resolve(__dirname, "../vite.config.ts")
-    )
-
-    return mergeConfig(config, {
-      ...userConfig,
-      // manually specify plugins to avoid conflict
-      plugins: [
-        svgrPlugin.default({
-          svgrOptions: {
-            icon: true
-          }
-        })
-      ]
-    })
-  },
-
   docs: {
-    autodocs: true
-  }
+    autodocs: "tag",
+  },
 }
+export default config
