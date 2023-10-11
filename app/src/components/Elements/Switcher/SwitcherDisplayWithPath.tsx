65cdee1b46562b70f5c0d85b3440eabcb89f86e6
inu_saru <inu_saru_eagle@yahoo.co.jp>
2023-10-11 23:43:10 +0900

    94a400ca add: SwitcherDisplayWithPath

---
+++ app/src/components/Elements/Switcher/SwitcherDisplayWithPath.tsx
@@ -0,0 +1,35 @@
+import { useLocation } from "react-router-dom"
+import { useDisclosure } from "@/hooks/useDisclosure"
+
+interface MethodsProps {
+  isOpenOnPath: () => boolean
+  openWithPath: (data: object) => void
+  closeWith: () => void
+}
+
+interface SwitcherDisplayWithPathProps {
+  children: (methods: MethodsProps) => React.ReactNode
+}
+
+export const SwitcherDisplayWithPath = ({
+  children
+}: SwitcherDisplayWithPathProps): JSX.Element => {
+  const { targetData, openWith, closeWith, isOpen } = useDisclosure()
+  const pathname = useLocation().pathname
+
+  const isOpenOnPath = (): boolean => {
+    return pathname === targetData.pathname && isOpen
+  }
+
+  const openWithPath = (): void => {
+    openWith({ pathname })
+  }
+
+  const methods = {
+    isOpenOnPath,
+    openWithPath,
+    closeWith
+  }
+
+  return <>{children(methods)}</>
+}
