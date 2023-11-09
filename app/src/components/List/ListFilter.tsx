5e86587ea457bdd3c1f96e7b1811c35e48950020
inu_saru <inu_saru_eagle@yahoo.co.jp>
2023-11-10 01:41:03 +0900

    7bd9bc2c update: set Chip for filter vocabulary_type

--- app/src/components/List/ListFilter.tsx
+++ app/src/components/List/ListFilter.tsx
@@ -1,4 +1,4 @@
-import { Search } from "@/components/Elements"
+import { Chip, Search } from "@/components/Elements"
 import { z } from "zod"
 
 const schema = z.object({
@@ -22,6 +22,37 @@ export const ListFilter = ({
           config={config}
           setSearchParams={setSearchParams}
         />
+        <div className="mt-2 flex gap-x-2">
+          <Chip
+            variant="secondary"
+            size="small"
+            onClick={() => {
+              const { vocabulary_type, ...other } = config
+              setSearchParams({ ...other })
+            }}
+            isActive={config.vocabulary_type === undefined}
+          >
+            全て
+          </Chip>
+          <Chip
+            variant="secondary"
+            onClick={() => {
+              setSearchParams({ ...config, vocabulary_type: "sentence" })
+            }}
+            isActive={config.vocabulary_type === "sentence"}
+          >
+            文章
+          </Chip>
+          <Chip
+            variant="secondary"
+            onClick={() => {
+              setSearchParams({ ...config, vocabulary_type: "word" })
+            }}
+            isActive={config.vocabulary_type === "word"}
+          >
+            単語
+          </Chip>
+        </div>
       </div>
     </>
   )
