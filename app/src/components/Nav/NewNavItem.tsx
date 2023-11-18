f9b1867b80586379747ba7ff0fe8fadc3fca0ea2
inu_saru <inu_saru_eagle@yahoo.co.jp>
2023-11-18 22:45:05 +0900

    7fd57482 WIP update: NavItem and NavItems

---
+++ app/src/components/Nav/NewNavItem.tsx
@@ -0,0 +1,34 @@
+import clsx from "clsx"
+
+interface NewNavItemProps {
+  children: React.ReactNode
+  isActive?: boolean
+  isEditing?: boolean
+  dropDown?: JSX.Element
+}
+
+export const NewNavItem = ({
+  children,
+  isActive = false,
+  isEditing = false,
+  dropDown
+}: NewNavItemProps): JSX.Element => {
+  return (
+    <li
+      className={clsx(
+        "h-8 hover:bg-primary-20 relative group list-none",
+        (isActive || isEditing) && "bg-primary-20"
+      )}
+    >
+      {children}
+      {dropDown !== undefined && (
+        <div className="absolute w-max inset-y-0 right-2 top-1 h-6 opacity-0 invisible group-hover:visible opacity-100">
+          {dropDown}
+        </div>
+      )}
+      {isEditing && (
+        <div className="ml-8 box-border border-b border-primary-200"></div>
+      )}
+    </li>
+  )
+}
---
+++ app/src/components/Nav/NewNavItems.tsx
@@ -0,0 +1,65 @@
+import { type UseInfiniteQueryResult } from "@tanstack/react-query"
+
+import { Spinner } from "@/components/Elements"
+import { type PagenateResponse } from "@/lib/react-query"
+import React from "react"
+import { useIntersectionObserver } from "@/hooks/useIntersectionObserver"
+
+interface NewNavItemsProps {
+  resourcesQuery: UseInfiniteQueryResult<PagenateResponse, unknown>
+  navItem: React.ReactElement
+}
+
+export const NewNavItems = ({
+  resourcesQuery,
+  navItem
+}: NewNavItemsProps): JSX.Element => {
+  const loadMoreButtonRef = React.useRef<HTMLButtonElement>(null)
+  useIntersectionObserver({
+    target: loadMoreButtonRef,
+    onIntersect: resourcesQuery.fetchNextPage,
+    enabled: resourcesQuery.hasNextPage
+  })
+
+  const navItemWith = ({ resource }: any): React.ReactElement => {
+    return React.cloneElement(navItem, resource)
+  }
+
+  if (resourcesQuery.isLoading) {
+    return (
+      <div className="py-4 w-full flex justify-center items-center">
+        <Spinner />
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <ul>
+        {resourcesQuery.data?.pages.map((page) =>
+          page.resources.map((resource, index) => (
+            <div key={index}>
+              {navItemWith({
+                resource
+              })}
+            </div>
+          ))
+        )}
+      </ul>
+      {/* FIXME: 初回レンダリング時にbuttonのrefが取得できないためinfinite scrollが反応しない。show allボタンを設置しているが理想はボタンが不要にしたい */}
+      {resourcesQuery.hasNextPage && (
+        <div>
+          {!resourcesQuery.isFetchingNextPage && (
+            <button
+              className="block mx-auto relative -mt-3 -mb-3 px-2 py-1 text-small border border-solid border-natural-50 rounded-xl bg-white hover:bg-primary-20"
+              ref={loadMoreButtonRef}
+              onClick={async () => await resourcesQuery.fetchNextPage()}
+            >
+              show all
+            </button>
+          )}
+        </div>
+      )}
+    </>
+  )
+}
