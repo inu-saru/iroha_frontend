9b0d03d936eef5847e4d5563ff47e015f5e5448e
inu_saru <inu_saru_eagle@yahoo.co.jp>
2023-11-21 23:26:08 +0900

    7fd57482 update: use NewNavItem on InnerSpaceNav

--- app/src/features/misc/components/InnerSpaceNav.tsx
+++ app/src/features/misc/components/InnerSpaceNav.tsx
@@ -1,8 +1,9 @@
+import clsx from "clsx"
 import { useNavigate } from "react-router-dom"
 
 import { useSpace } from "@/features/spaces/api/getSpace"
 import { Button, Icon, Link } from "@/components/Elements"
-import { NavItem } from "@/components/Nav"
+import { NewNavItem } from "@/components/Nav/NewNavItem"
 import { NavItemsSection } from "@/features/sections"
 
 import { useUrlParams } from "@/lib/useUrlParams"
@@ -35,13 +36,17 @@ export const InnerSpaceNav = (): JSX.Element => {
       >
         新規作成
       </Button>
-      <NavItem
-        isActive={searchParams.get("sid") === null}
-        resourceId="all"
-        label="全てのセクション"
-        to={`/app/spaces/${spaceId}/vocabularies`}
-        icon="all"
-      />
+      <NewNavItem isActive={searchParams.get("sid") === null}>
+        <Link
+          to={`/app/spaces/${spaceId}/vocabularies`}
+          className={clsx("block py-1 px-2 truncate")}
+        >
+          <Icon className="float-left" variant="all" />
+          <span className="ml-1 text-h200 text-natural-900 overflow-hidden">
+            全てのセクション
+          </span>
+        </Link>
+      </NewNavItem>
       <NavItemsSection />
     </div>
   )
