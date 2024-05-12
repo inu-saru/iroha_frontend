2ab0865dd7f87a3dfe67393942f123d0e3bd7674
inu_saru <inu_saru_eagle@yahoo.co.jp>
2024-05-12 13:48:32 +0700

    48ee7427 update: set Outlet on List Layout component

--- app/src/features/vocabularies/routes/index.tsx
+++ app/src/features/vocabularies/routes/index.tsx
@@ -4,14 +4,16 @@ import { Vocabulary } from "./Vocabulary"
 import { VocabularyCreate } from "./VocabularyCreate"
 import { Vocabularies } from "./Vocabularies"
 import { VocabularyUpdate } from "./VocabularyUpdate"
-
+ 
 export const VocabularyRoutes = (): JSX.Element => {
   return (
     <Routes>
-      <Route path=":vocabularyId/edit" element={<VocabularyUpdate />} />
-      <Route path=":vocabularyId" element={<Vocabulary />} />
-      <Route path="new" element={<VocabularyCreate />} />
-      <Route path="" element={<Vocabularies />} />
+      <Route element={<Vocabularies />}>
+        <Route path="" element={null} />
+        <Route path="new" element={<VocabularyCreate />} />
+        <Route path=":vocabularyId" element={<Vocabulary />} />
+        <Route path=":vocabularyId/edit" element={<VocabularyUpdate />} />
+      </Route>
     </Routes>
   )
 }
