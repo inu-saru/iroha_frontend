cdcc63178a5f61bf022acbb7b2d4ed56127aac21
inu_saru <inu_saru_eagle@yahoo.co.jp>
2025-05-25 18:43:22 +0700

    17f0e64e update: Form/Input

--- app/src/components/Elements/Form/Input.tsx
+++ app/src/components/Elements/Form/Input.tsx
@@ -2,20 +2,24 @@ import { type FieldError, type UseFormRegisterReturn } from "react-hook-form"
 
 interface InputProps {
   type?: "text" | "email" | "password"
-  className?: string
   label: string
+  className?: string
+  placeholder?: string
+  maxLength?: number
   registration: Partial<UseFormRegisterReturn>
   error?: FieldError | undefined | any
 }
 
 export const Input = (props: InputProps): JSX.Element => {
-  const { type = "text", label, className, registration, error } = props
+  const { type = "text", label, className, placeholder = "", maxLength, registration, error } = props
   return (
     <div className={className}>
       <label className="block text-h200 text-natural-900">{label}:</label>
       <input
         type={type}
         className="block w-full text-default mt-2 px-4 py-3 text-natural-900 border border-natural-40 bg-white focus:ring-primary-100 focus:border-primary-100"
+        placeholder={placeholder}
+        maxLength={maxLength}
         {...registration}
       />
       <p className="pl-4 pr-2 pt-1 pb-3 font-small text-error-300">
